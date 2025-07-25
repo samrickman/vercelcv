e9fdd9888bf7b8055205348d38d371f1cfe9d12b
samrickman <samrickman+githubpro@gmail.com>
2025-07-25 09:25:29 +0100

    fix: render compiled contentlayer

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -4,17 +4,17 @@ export const Blog = defineDocumentType(() => ({
   name: 'Blog',
   filePathPattern: `blog/*.mdx`,
   contentType: 'mdx',
-  bodyType: 'mdx',
+  bodyType: 'mdx',                     // compiled component + code + html
   fields: {
     title:   { type: 'string', required: true },
     date:    { type: 'date',   required: true },
     summary: { type: 'string' },
   },
   computedFields: {
-    /** slug = file name without directory or extension */
     slug: {
       type: 'string',
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx?$/, ''),
+      resolve: (doc) =>
+        doc._raw.sourceFileName.replace(/\.mdx?$/, ''),
     },
   },
 }))
