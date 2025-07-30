90ccd404a75a72d2c64d4708aa2716c0bd93d138
samrickman <samrickman+githubpro@gmail.com>
2025-07-30 15:04:01 +0100

    content: updated portrait to more current image

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -5,7 +5,7 @@ export default function PageHeader() {
 
     <header className="pt-20 p-6 text-center flex flex-col items-center">
       <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-gray-600 mb-4 image-container">
-        <Image src="/portrait4.png" alt="Sam Rickman" width={128} height={128} className="object-cover" />
+        <Image src="/portrait6.png" alt="Sam Rickman" width={128} height={128} className="object-cover" />
       </div>
       <h1 className="text-4xl font-bold">Sam Rickman PhD</h1>
       <p className="text-lg">Data Scientist | Evaluating LLMs | AI in Public Services</p>
